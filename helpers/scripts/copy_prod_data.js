f6a94885b63f7ff87585d217811bb1e1edfd6f6b
Stephen Cholvat <Scholvat@uwaterloo.ca>
2018-12-24 10:34:47 -0800

    Added login to script

--- helpers/scripts/copy_prod_data.js
+++ helpers/scripts/copy_prod_data.js
@@ -4,6 +4,13 @@ var VERSION = '1.5'
 
 const fetch = require("node-fetch");
 const mongoose = require('mongoose');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
 
 function get(endpoint, headers) {
 	return fetch(`${PROD_ADDRESS}${endpoint}`, {
@@ -23,55 +30,89 @@ function get(endpoint, headers) {
 		});
 }
 
+function post(endpoint, headers, body) {
+	return fetch(`${PROD_ADDRESS}${endpoint}`, {
+		method: 'POST',
+		headers: {
+			Accept: 'application/json',
+			'Content-Type': 'application/json',
+			...headers,
+		},
+		body: JSON.stringify(body),
+	})
+	.catch(err => {
+		err = err.replace(/</g, '').replace(/>/g, '');
+		console.error(err);
+	});
+}
 
-mongoose.connect('mongodb://'+DEV_ADDRESS+'/grapp-dev');
 
-get('/users/', {}).then(users =>{
-  require('../../models/User');
-  const Usermodel = mongoose.model('User');
-  const promises = users.map(data => {
-    const user = new Usermodel(data);
-    return user.changePassword('password').then(updatedUser => {
-      user.save().then(user => {})
-      .catch(err => {
-        console.log(`Error saving ${user} ${err}`)
-      });
-    });
-  });
-  console.log("Setting user data")
-  Promise.all(promises).then(results => {
-    console.log("Successfully populated user data")
-  });
-});
+rl.question('Username: ', (username) => {
+	rl.question('Password: ', (password) => {
+		mongoose.connect('mongodb://'+DEV_ADDRESS+'/grapp-dev');
+		var token;
+		post('/users/login', {}, {
+			username: username,
+			password: password,
+		}).then(response => response.json())
+		.then(jsonResponse => {
+			if(jsonResponse.err || !jsonResponse.token){
+				console.error("Could not login");
+				process.exit();
+			}else{
+				token = jsonResponse.token;
+				console.log("Successfully logged in");
+			}
+		});
 
-get('/channels/', {}).then(items =>{
-	require('../../models/Channels');
-	const model = mongoose.model('Channel');
-	const promises = items.map(data => {
-		const item = new model(data);
-		return item.save().then(item => {})
-			.catch(err => {
-				console.log(`Error saving ${item} ${err}`)
+		get('/users/', {'Authorization': 'Bearer ' + token}).then(users =>{
+			require('../../models/User');
+			const Usermodel = mongoose.model('User');
+			const promises = users.map(data => {
+				const user = new Usermodel(data);
+				return user.changePassword('password').then(updatedUser => {
+					user.save().then(user => {})
+					.catch(err => {
+						console.log(`Error saving ${user} ${err}`)
+					});
+				});
 			});
-	});
-	console.log("Setting channel data")
-	Promise.all(promises).then(results => {
-		console.log("Successfully populated channel data")
-	});
-});
-
-get('/posts/', {}).then(items =>{
-	require('../../models/Posts');
-	const model = mongoose.model('Post');
-	const promises = items.map(data => {
-		const item = new model(data);
-		return item.save().then(item => {})
-			.catch(err => {
-				console.log(`Error saving ${item} ${err}`)
+			console.log("Setting user data")
+			Promise.all(promises).then(results => {
+				console.log("Successfully populated user data")
 			});
-	});
-	console.log("Setting post data")
-	Promise.all(promises).then(results => {
-		console.log("Successfully populated post data")
-	});
-});
+		});
+
+		get('/channels/', {'Authorization': 'Bearer ' + token}).then(items =>{
+			require('../../models/Channels');
+			const model = mongoose.model('Channel');
+			const promises = items.map(data => {
+				const item = new model(data);
+				return item.save().then(item => {})
+					.catch(err => {
+						console.log(`Error saving ${item} ${err}`)
+					});
+			});
+			console.log("Setting channel data")
+			Promise.all(promises).then(results => {
+				console.log("Successfully populated channel data")
+			});
+		});
+
+		get('/posts/', {'Authorization': 'Bearer ' + token}).then(items =>{
+			require('../../models/Posts');
+			const model = mongoose.model('Post');
+			const promises = items.map(data => {
+				const item = new model(data);
+				return item.save().then(item => {})
+					.catch(err => {
+						console.log(`Error saving ${item} ${err}`)
+					});
+			});
+			console.log("Setting post data")
+			Promise.all(promises).then(results => {
+				console.log("Successfully populated post data")
+			});
+		});
+	})
+})
