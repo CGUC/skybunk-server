d768c50c78f3ebe8aabfa8b45c4420164cbb81cd
Ryan Martin <martr4929@hotmail.com>
2019-05-15 12:07:38 -0400

    Add eslint config and update repo to satisfy it (#91)
    * Add eslint config and update repo
    
    * Fix build config
    
    * update gitignore

--- app.js
+++ app.js
@@ -10,16 +10,17 @@ const app = express();
 // Connect to mongodb
 // connect returns a Promise object - get used to these.
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
-// Due to the asyrnchonous nature of nodeJs, a promise allows us to safely execute code only 
+// Due to the asyrnchonous nature of nodeJs, a promise allows us to safely execute code only
 // once a certain operation has finished
 // For example, in this case the function passed into the .then clause is only
 // executed once the .connect(...) call has finished.
-// If something fails within the .connnect(...) call then the code passed in to the 
+// If something fails within the .connnect(...) call then the code passed in to the
 // .catch clause is executed instead.
 const db = require('./config/database');
+
 mongoose.connect(db.mongoURI)
-.then(() => console.log('MongoDB Connected...'))
-.catch(err => console.log(err));
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
 
 // Initialize the Middleware for our imported node modules
 // Middleware is a function(s) that occur before every HTTP request
@@ -31,36 +32,41 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Set preflight headers to allows cors
 app.use('*', (req, res, next) => {
-	res.header('Access-Control-Allow-Headers', 'content-type, page, authorization');
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Methods', 'PUT, DELETE')
-	next();
+  res.header('Access-Control-Allow-Headers', 'content-type, page, authorization');
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'PUT, DELETE');
+  next();
 });
 
 // Direct the appropriate urls to the respective controller (you may see this referred
 // to as routes & routers elsewhere)
 // The first parameter passed into app.use(...) directs all urls starting
 // with this parameter to the controller passed in to the second paramater.
-// Here, every request from the uri ('/examples/<ANYTHING>') will check the exampleController for the matching
-// <ANYTHING> route
+// Here, every request from the uri ('/examples/<ANYTHING>') will check the exampleController
+// for the matching <ANYTHING> route
 const exampleController = require('./controllers/exampleController');
+
 app.use('/examples', exampleController);
 
 const channelController = require('./controllers/channelController');
+
 app.use('/channels', channelController);
 
 const userController = require('./controllers/userController');
+
 app.use('/users', userController);
 
 const postController = require('./controllers/postController');
+
 app.use('/posts', postController);
 
 const notificationsController = require('./controllers/notificationsController');
+
 app.use('/notifications', notificationsController);
 
 // Start the server on port 3000
 // Use the port environment variable (needed for heroku) or 300 (dev)
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-	console.log(`Server started on port ${port}`);
+  console.log(`Server started on port ${port}`);
 });
--- helpers/notificationManager.js
+++ helpers/notificationManager.js
@@ -1,34 +1,47 @@
+require('../models/Channels');
 const mongoose = require('mongoose');
 const Expo = require('expo-server-sdk');
-let expo = new Expo();
+
+const expo = new Expo();
 
 require('../models/Notification');
+
 const Notification = mongoose.model('Notification');
 
 module.exports = {
-  sendNotifications: async function(messages) {
-	  let chunks = expo.chunkPushNotifications(messages);
-	  let tickets = [];
-
-    for(const chunk of chunks) {
-	    try {
-	      let ticketChunk = await expo.sendPushNotificationsAsync(chunk);
-	      tickets.push(...ticketChunk);
-	    } catch (error) {
-	      console.error(error);
-	    }
-  	}
+  dispatchNotifications(post, author) {
+    const Channel = mongoose.model('Channel');
+    Channel.findByTags(post.tags).then((channels) => {
+      channels.forEach((channel) => {
+        channel.notifyUsersOfPost(post, author);
+      });
+    }).catch(err => console.log(err));
   },
-  saveNotificationForUserAsync: async function (notificationData, user) {
-  	const notification = new Notification(notificationData);
-  	notification.save().then(notif => {
-  		user.notifications.unshift(notif);
+
+  async sendNotifications(messages) {
+    const chunks = expo.chunkPushNotifications(messages);
+    const tickets = [];
+
+    for (const chunk of chunks) {
+      try {
+        const ticketChunk = await expo.sendPushNotificationsAsync(chunk);
+        tickets.push(...ticketChunk);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  },
+
+  async saveNotificationForUserAsync(notificationData, user) {
+    const notification = new Notification(notificationData);
+    notification.save().then((notif) => {
+      user.notifications.unshift(notif);
       const removed = user.notifications.splice(30);
-  		user.save().then(user => {
+      user.save().then(() => {
         Notification.deleteMany({ _id: { $in: removed } });
       })
-  		.catch(err => console.error(err));
-  	})
-  	.catch(err => console.error(err));
-  }
-}
+        .catch(err => console.error(err));
+    })
+      .catch(err => console.error(err));
+  },
+};
