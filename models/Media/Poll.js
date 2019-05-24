2dbfbaf33a5c4e79062788eea8dbce661b9d0e9d
Ryan Martin <martr4929@hotmail.com>
2019-05-24 12:39:07 -0400

    Add endpoints to manage polls on a post

--- models/Media/Poll.js
+++ models/Media/Poll.js
@@ -106,7 +106,13 @@ PollSchema.methods.addOption = function (option, userId) {
 PollSchema.methods.placeVote = function (userId, optionId) {
   return new Promise((resolve, reject) => {
     const option = this.options.id(optionId);
-    if (option.usersVoted.some(u => u.toString() === userId.toString())) {
+    
+    if (!option) {
+      reject(Error('Option not found'));
+      return;
+    }
+
+    if (option.usersVoted.some((u) => u.toString() == userId.toString())) {
       reject(Error('User has already voted for this option'));
       return;
     }
@@ -128,6 +134,10 @@ PollSchema.methods.placeVote = function (userId, optionId) {
 PollSchema.methods.retractVote = function (userId, optionId) {
   return new Promise((resolve, reject) => {
     const option = this.options.id(optionId);
+    if (!option) {
+      reject(Error('Option not found'));
+      return;
+    }
 
     option.set({
       text: option.text,
