7cd2f4acad5df94c7e5e791a5e57f6db3825604d
Ryan Martin <martr4929@hotmail.com>
2019-08-12 11:32:34 -0400

    Add admin ednpoint for inviting users via email

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,13 +1,21 @@
 const express = require('express');
 
 const router = express.Router();
-
 const adminHelper = require('../helpers/adminHelper');
+const { verifyToken, verifyAdmin } = require('../helpers/authorization');
 
-
-router.post('/tickets', (req, res) => {
+router.post('/tickets', verifyToken, verifyAdmin, (req, res) => {
   adminHelper.generateTickets(req.body.count).then((tickets) => {
     res.json(tickets);
+  }).catch((err) => {
+    console.error(err);
+    res.status(500).send('error');
+  });
+});
+
+router.post('/inviteUsers', verifyToken, verifyAdmin, (req, res) => {
+  adminHelper.inviteUsers(req.body.users).then((status) => {
+    res.json(status);
   }).catch((err) => {
     console.error(err);
     res.status(500).send('error');
