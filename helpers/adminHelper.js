86ad251054281990114e004423fd7a08cab1677f
Ryan Martin <martr4929@hotmail.com>
2019-08-13 12:58:01 -0400

    Add admin ednpoint for inviting users via email (#100)

--- helpers/adminHelper.js
+++ helpers/adminHelper.js
@@ -1,11 +1,45 @@
+require('../models/GoldenTicket');
 const mongoose = require('mongoose');
+const nodemailer = require('nodemailer');
 const fetch = require('node-fetch');
 const { authServerAddress } = require('../config/options');
 const { accessKey } = require('../config/secrets');
 
-require('../models/GoldenTicket');
-
 const GoldenTicket = mongoose.model('GoldenTicket');
+
+function dispatchEmail(user, ticket, transporter) {
+  const mailOptions = {
+    from: process.env.EMAIL_ADDRESS,
+    to: user.email,
+    subject: 'Welcome to Skybunk!',
+    html: `
+      <p>Hi ${user.name},</p>
+      <h2>You have been invited to Skybunk!</h2>
+      <p>Skybunk is a social platform in order to facilitate community within University Residences. You can meet others in your residence,
+      stay on top of important happenings, and organize events of your own.</p>
+      <p>Please register using the following golden ticket: ${ticket}</p>
+      <h4>Download the app!</h4>
+      <p>Android: <a href="https://play.google.com/store/apps/details?id=com.grebel.skybunk&hl=en_CA">https://play.google.com/store/apps/details?id=com.grebel.skybunk&hl=en_CA</a><br></br>
+      iOS: <a href="https://apps.apple.com/ca/app/skybunk/id1411727712">https://apps.apple.com/ca/app/skybunk/id1411727712</a></p>
+    `,
+  };
+
+  return new Promise((resolve) => {
+    transporter.sendMail(mailOptions, (err) => {
+      const response = {
+        errors: [],
+        successes: 0,
+      };
+
+      if (err) {
+        response.errors.push({ user: user.email, err });
+      } else {
+        response.successes = 1;
+      }
+      resolve(response);
+    });
+  });
+}
 
 module.exports.generateTickets = count => fetch(`${authServerAddress}/servers/${process.env.SERVER_ID}/tickets`, {
   method: 'POST',
@@ -17,7 +51,36 @@ module.exports.generateTickets = count => fetch(`${authServerAddress}/servers/${
   body: JSON.stringify({ count }),
 }).then(response => response.json()).then(async (jsonResponse) => {
   await GoldenTicket.insertMany(jsonResponse.map(ticketNumber => ({ ticketNumber })));
+
   return jsonResponse;
 }).catch((err) => {
   console.error(err);
 });
+
+module.exports.inviteUsers = async (users) => {
+  let tickets = [];
+  try {
+    tickets = await module.exports.generateTickets(users.length);
+  } catch (e) {
+    return { errors: { err: 'Could not generate golden tickets.' } };
+  }
+
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL_ADDRESS,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
+
+  const promises = [];
+  users.forEach((u, i) => {
+    promises.push(dispatchEmail(u, tickets[i], transporter));
+  });
+
+  return Promise.all(promises).then(values => values.reduce((acc, val) => {
+    acc.errors = [...acc.errors, ...val.errors];
+    acc.successes += val.successes;
+    return acc;
+  }, { errors: [], successes: 0 }));
+};
