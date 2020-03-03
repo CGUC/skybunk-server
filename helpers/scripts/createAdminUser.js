2af2f88c286b7a2b4ff2e7420d2810fbb1184aa8
Matthew Pollex <matthew.pollex@gmail.com>
2020-03-03 15:04:55 -0500

    Improve createAdminUser setup script  - Prompt user for values

--- helpers/scripts/createAdminUser.js
+++ helpers/scripts/createAdminUser.js
@@ -1,19 +1,52 @@
 require('../../models/User');
 
+const inquirer = require('inquirer');
 const fetch = require('node-fetch');
 const { generateTickets } = require('../adminHelper');
-const authServerAddress = process.env.AUTH_SERVER_URI || 'https://skybunk-auth-dev.herokuapp.com';
+const authServerAddress = process.env.AUTH_SERVER_URI;
 const { mongoURI } = require('../../config/database');
 
 const mongoose = require('mongoose');
 const GoldenTicket = mongoose.model('GoldenTicket');
 const User = mongoose.model('User');
 
-const adminUser = {
-    username: 'admin',
-    password: 'admin',
-    firstName: 'Admin',
-    lastName: 'Admin',
+const promptForText = async ({ type = 'input', message, defaultValue, validate = () => true }) => {
+    const { res } = await inquirer.prompt({
+        type,
+        name: 'res',
+        message,
+        default: defaultValue,
+        validate
+    });
+    return res;
+};
+
+const getExistingUsers = async () => {
+    const rawResponse = await fetch(`${authServerAddress}/users`, {
+        method: 'GET',
+        headers: {
+            Accept: 'application/json',
+        }
+    });
+    return (await rawResponse.json()).map(({ username }) => username);
+};
+
+const buildAdminUser = async () => {
+    let existingUsers;
+    try {
+        existingUsers = await getExistingUsers();
+    } catch (err) {
+        console.error(`An auth server does not appear to be running at '${authServerAddress}'`);
+        return;
+    }
+
+    const validateUsername = (username) => !existingUsers.includes(username) || 'A user with that username already exists';
+    const username = await promptForText({ message: 'Enter new username', validate: validateUsername });
+    const password = await promptForText({ type: 'password', message: 'Enter password'});
+    const firstName = await promptForText({ message: 'Enter first name:', defaultValue: 'Admin'});
+    const lastName = await promptForText({ message: 'Enter last name:', defaultValue: 'Admin'});
+
+    return { username, password, firstName, lastName };
 };
 
 const createUserOnAuth = ({ username, password, goldenTicket }) =>
@@ -40,12 +73,18 @@ const createUserOnAuth = ({ username, password, goldenTicket }) =>
         return;
     }
 
+    const adminUser = await buildAdminUser();
+    if (!adminUser) return;
+
+    console.log('Initializing MongoDB');
     await mongoose.connect(mongoURI);
 
     //Create a golden ticket
+    console.log('Creating a golden ticket');
     const tickets = await generateTickets(1);
 
     // Create user on auth server
+    console.log('Creating the user on auth server');
     const { errmsg } = await createUserOnAuth({ ...adminUser, goldenTicket: tickets[0] });
     if (errmsg) {
         console.error(errmsg);
@@ -53,8 +92,11 @@ const createUserOnAuth = ({ username, password, goldenTicket }) =>
     }
 
     // Create user locally (may be removed when/if auth server handles it instead)
+    console.log('Creating user locally');
     await User.create({ ...adminUser, goldenTicket: tickets[0], role: ['admin'] });
     await GoldenTicket.deleteOne({ ticketNumber: tickets[0] });
 
+    console.log('Disconnecting from MongoDB');
     await mongoose.disconnect();
+    console.log('Finished. User created successfully');
 })();
